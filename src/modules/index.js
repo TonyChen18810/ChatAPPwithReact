5bf431167d0dbf675200154b5d2ba6a3f7b47d0c
TonyChen18810 <tonychen1880@gmail.com>
2020-07-07 22:48:56 +0800

    end

--- src/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import chart from './chart';
-
-export default combineReducers({
-  router: routerReducer,
-  chart
-});
